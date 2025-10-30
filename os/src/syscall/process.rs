5a71b6187cea17da5f5739591b4f569c7a81be8d
vegetablechicken-dev <1204694597@qq.com>
2025-10-31 04:10:45 +0800

    ch6 finish sys_get_time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,11 +1,14 @@
 use crate::{
     fs::{open_file, OpenFlags},
-    mm::{translated_ref, translated_refmut, translated_str},
+    mm::{translated_ref, translated_refmut, translated_str, translated_byte_buffer},
     task::{
         current_process, current_task, current_user_token, exit_current_and_run_next, pid2process,
         suspend_current_and_run_next, SignalFlags,
     },
+    timer::get_time_us,
 };
+
+use core::{mem::size_of, slice, ptr, };
 use alloc::{string::String, sync::Arc, vec::Vec};
 
 #[repr(C)]
@@ -146,17 +149,37 @@ pub fn sys_kill(pid: usize, signal: u32) -> isize {
     }
 }
 
-/// get_time syscall
-///
-/// YOUR JOB: get time with second and microsecond
-/// HINT: You might reimplement it with virtual memory management.
-/// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
+
+/// Get time with second and microsecond
+/// You might reimplement it with virtual memory management.
+/// What if [`TimeVal`] is splitted by two pages ?
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     trace!(
-        "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
-        current_task().unwrap().process.upgrade().unwrap().getpid()
+        "kernel: sys_get_time IMPLEMENTED",
     );
-    -1
+    let time_val_size = size_of::<TimeVal>();
+    let buffers = translated_byte_buffer(current_user_token(),
+                                         ts as *const u8,
+                                         time_val_size);
+    let us = get_time_us();
+    let time_val = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+    let mut cur = 0;
+    let time_slice = unsafe {
+        slice::from_raw_parts(&time_val as *const TimeVal as *const u8, time_val_size)
+    };
+    for buffer in buffers {
+        let write_len = buffer.len().min(time_slice.len() - cur);
+        unsafe {
+            ptr::copy(&time_slice[cur..] as *const _ as *const u8,
+                      buffer as *mut _ as *mut u8,
+                      write_len);
+        }
+        cur += write_len;
+    }
+    0
 }
 
 /// mmap syscall
