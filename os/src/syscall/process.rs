3662eefdacc3318955f1ae2ed27902f585d4b1f9
vegetablechicken-dev <1204694597@qq.com>
2025-10-31 19:33:16 +0800

    ch6 cherry-pick sys_get_time -Test sys_get_time successfully

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -4,7 +4,7 @@ use alloc::sync::Arc;
 
 use crate::{
     fs::{open_file, OpenFlags},
-    mm::{translated_refmut, translated_str},
+    mm::{translated_refmut, translated_str, translated_byte_buffer},
     task::{
         add_task, current_task, current_user_token, exit_current_and_run_next,
         suspend_current_and_run_next,
